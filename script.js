991b6b310933df00d27fdf30fa1aaf7b65d0db91
HayesHiller <hayeshiller@hotmail.com>
2020-11-07 12:44:56 -0700

    fixing bug on my work slide/button

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-function showabout() {
-    $("#about_container").css("display", "inherit");
+function showabout(){
+    $("#about_container").css("display","inherit");
     $("#about_container").addClass("animated slideInLeft");
     setTimeout(function(){
         $("#about_container").removeClass("animated slideInLeft");
     },800);
 }
-function closeabout() {
+function closeabout(){
     $("#about_container").addClass("animated slideOutLeft");
     setTimeout(function(){
         $("#about_container").removeClass("animated slideOutLeft");
-        $("#about_container").css("display", "none");
+        $("#about_container").css("display","none");
     },800);
 }
-function showwork() {
-    $("#work_container").css("display", "inherit");
+function showwork(){
+    $("#work_container").css("display","inherit");
     $("#work_container").addClass("animated slideInRight");
     setTimeout(function(){
         $("#work_container").removeClass("animated slideInRight");
     },800);
 }
-function closework() {
+function closework(){
     $("#work_container").addClass("animated slideOutRight");
     setTimeout(function(){
         $("#work_container").removeClass("animated slideOutRight");
-        $("#wrk_container").css("display", "none");
+        $("#work_container").css("display","none");
     },800);
 }
-function showContact() {
-    $("#contact_container").css("display", "inherit");
+function showcontact(){
+    $("#contact_container").css("display","inherit");
     $("#contact_container").addClass("animated slideInUp");
     setTimeout(function(){
         $("#contact_container").removeClass("animated slideInUp");
     },800);
 }
-function closecontact() {
+function closecontact(){
     $("#contact_container").addClass("animated slideOutDown");
     setTimeout(function(){
         $("#contact_container").removeClass("animated slideOutDown");
@@ -43,7 +43,7 @@ function closecontact() {
 setTimeout(function(){
     $("#loading").addClass("animated fadeOut");
     setTimeout(function(){
-        $("#loading").removeClass("animated fadeOut");
+      $("#loading").removeClass("animated fadeOut");
       $("#loading").css("display","none");
       $("#box").css("display","none");
       $("#about").removeClass("animated fadeIn");
